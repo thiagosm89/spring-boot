d3a7764b07ed368be7a1279ad788e383716f7898
MarVinCar <marvin.cardoso84@gmail.com>
2022-01-17 21:45:00 -0300

    feat(PessoaController): add inserirPessoa

--- src/main/java/packages/controller/PessoaController.java
+++ src/main/java/packages/controller/PessoaController.java
@@ -1,8 +1,27 @@
 package packages.controller;
 
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import packages.domain.Pessoa;
+import packages.dto.PessoaInsertionDTO;
 
-@RestController("/pessoa")
+@RestController
+@RequestMapping("/pessoa")
 public class PessoaController {
+
+@Autowired
+    ModelMapper modelMapper;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public void inserirPessoa (PessoaInsertionDTO pessoaInsertionDTO){
+        Pessoa pessoa = new Pessoa();
+        pessoa = modelMapper.map(pessoaInsertionDTO, Pessoa.class);
+    }
 
 }
