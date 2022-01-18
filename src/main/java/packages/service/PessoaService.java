b38ce03dd0fb861bdefef12fa6383ec3e76a8537
MarVinCar <marvin.cardoso84@gmail.com>
2022-01-18 13:42:18 -0300

    feat(PessoaService): add PessoaService

---
+++ src/main/java/packages/service/PessoaService.java
@@ -0,0 +1,21 @@
+package packages.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import packages.domain.Pessoa;
+import packages.repository.PessoaRepository;
+
+@Service
+public class PessoaService {
+
+    private final PessoaRepository pessoaRepository;
+
+    @Autowired
+    public PessoaService(PessoaRepository pessoaRepository) {
+        this.pessoaRepository = pessoaRepository;
+    }
+
+    public Pessoa inserirPessoa(Pessoa pessoa){
+        return pessoaRepository.save(pessoa);
+    }
+}
